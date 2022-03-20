6398df3faf9a8eb6bdc0f3291cff51e1d5ad7459
hgsgtk <kazuki@autify.com>
2022-03-20 14:34:05 +0900

    test: ExampleTest

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+final class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testGotoPage(): void
+    {
+        $this->assertTrue(true);
+    }
+}
