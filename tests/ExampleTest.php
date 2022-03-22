c17b81e7b0babd9a92aa70592466cb922324597e
hgsgtk <kazuki@autify.com>
2022-03-22 20:25:37 +0900

    feat: initialize classes Client and tab
    Client is a client code to send HTTP requests to remote debbuging interfaces.
    Tab is a class that contains the state of Tab.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
+use PhpCdp\Client;
+
 final class ExampleTest extends \PHPUnit\Framework\TestCase
 {
     public function testGotoPage(): void
     {
+        $client = new Client();
+        $tab = $client->openTab();
         $this->assertTrue(true);
     }
 }
