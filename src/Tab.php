ad2ba1a2664cfe6130189cd19bf545ab04871846
hgsgtk <kazuki@autify.com>
2022-03-24 15:26:33 +0900

    feat: close a tab

--- src/Tab.php
+++ src/Tab.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace PhpCdp;
 
+use \GuzzleHttp\ClientInterface;
+
 final class Tab
 {
     /**
      * 
      */
-    public function __construct()
+    public function __construct(
+        private string $id,
+        private string $host,
+        private string $port,
+        private ClientInterface $httpClient,
+    )
     {
+    }
+
+    public function close() 
+    {
+        $response = $this->httpClient->request(
+            'GET', "http://{$this->host}:{$this->port}/json/close/{$this->id}");
+        // Fixme: error handling
     }
 }
